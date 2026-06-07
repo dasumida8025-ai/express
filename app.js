e3573bf15ce7bcc512a17792c9627a3832251f97
角田凜太朗 <sumidarintarou@tsunodarintarounoMacBook-Air.local>
2026-06-07 22:33:59 +0900

    add cat api

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var nodesRouter = require('./routes/nodes');
 var nodesRouter = require('./routes/notes');
+var catRouter = require('./routes/cat');
 
 var app = express();
 
@@ -27,7 +28,8 @@ app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/nodes', nodesRouter);
-app.use('/notes', nodesRouter);
+// app.use('/notes', notesRouter);
+app.use('/cat', catRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
